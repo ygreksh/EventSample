1d59aded67d31660888fc140c9a197776cd3eaed
ygrek <ygreksh@gmail.com>
2021-08-11 10:31:51 +0300

    notify

---
+++ EventSample/Account.cs
@@ -0,0 +1,34 @@
+﻿namespace EventSample
+{
+    public class Account
+    {
+        public delegate void AccountHandler(string message);
+
+        public event AccountHandler Notify;
+        public Account(int sum)
+        {
+            Sum = sum;
+        }
+        // сумма на счете
+        public int Sum { get; private set;}
+        // добавление средств на счет
+        public void Put(int sum)    
+        {
+            Sum += sum;
+            Notify?.Invoke($"Счет пополнен на {sum}. Итого: {Sum}");
+        }
+        // списание средств со счета
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke($"Счет уменьшен на {sum}. Итого: {Sum}");
+            }
+            else
+            {
+                Notify?.Invoke($"На счету недостаточно денег для снятия. Текущий баланс {Sum}");
+            }
+        }
+    }
+}
---
+++ EventSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EventSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(100);
+            //acc.Notify += new Account.AccountHandler(DisplayRedMessage);//делегат в качестве обработчика события
+            //acc.Notify += DisplayMessage;   //метод в качестве обработчика события
+            /*
+            acc.Notify += delegate (string mes)     //анонимный метод в качестве обработчика события
+            {
+                Console.WriteLine(mes);
+            };
+            */
+
+            acc.Notify += mes => Console.WriteLine(mes);    //лямбда в качестве обработчика события
+            //acc.Notify += DisplayRedMessage;
+            acc.Put(20);    // добавляем на счет 20
+            //Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            acc.Take(70);   // пытаемся снять со счета 70
+            //Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            acc.Take(180);  // пытаемся снять со счета 180
+            //Console.WriteLine($"Сумма на счете: {acc.Sum}");
+        }
+        /*
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        */
+        /*
+        private static void DisplayRedMessage(String message)
+        {
+            // Устанавливаем красный цвет символов
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            // Сбрасываем настройки цвета
+            Console.ResetColor();
+        }*/
+    }
+}
